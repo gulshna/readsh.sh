42e1c61c51dc72af6072d38687ffb12ed69ca580
gulshna <39676765+gulshna@users.noreply.github.com>
2019-06-06 17:51:06 +0530

    Create readsh.sh

---
+++ readsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "this is repo1 test"
+echo "this is  repo2 test"
+echo "there is new parmeter $PASSWD"
+NUM=$(($FIRSTNUM + $SECONDNUM))
+echo "$NUM"
+echo "it is the sum test"
+echo "Hi welcome"
